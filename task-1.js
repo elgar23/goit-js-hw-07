7086ed2499934a44a1db90553ea1e110b621cae1
elgar23 <superel2303@gmail.com>
2020-12-22 15:52:22 +0200

    работа над ошибками

--- task-1.js
+++ task-1.js
@@ -1,10 +1,17 @@
-const totalCategories = document.querySelectorAll(".item");
+const items = document.querySelectorAll('#categories .item');
+console.log(`В списке ${items.length} категории.`);
+Array.prototype.map.call(items, element => {
+  const title = element.querySelector('h2').innerHTML;
+  const itemsLength = element.querySelectorAll('li').length;
+  console.log(`Категория: ${title} \nКоличество элементов: ${itemsLength}`);
+});
+
+/*const totalCategories = document.querySelectorAll(".item");
 console.log(`В списке ${totalCategories.length} категории.`);
 
 const categoriesArray = [...totalCategories]
-  .map(
-    (categories) => `Категория: ${categories.children[0].textContent}
+  .map((categories) => `Категория: ${categories.children[0].textContent}
 Количество элементов: ${categories.children[1].children.length}`
   )
   .join("\n");
-console.log(categoriesArray);
+console.log(categoriesArray);*/
--- task-2.js
+++ task-2.js
@@ -7,12 +7,21 @@ const ingredients = [
   "Приправы",
 ];
 
-const ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  return item;
+});
+const list = document.querySelector('#ingredients');
+list.append(...foodIngredients);
+console.log(list);
+
+/*const ulList = document.getElementById("ingredients");
 const foodIngredients = ingredients.map((ingredient) => {
   const items = document.createElement("li");
-  items.textContent = ingredient;
+  items.textContent = [ingredient];
 
   return ulList.append(items);
 });
 
-console.log(ulList);
+console.log(ulList);*/
